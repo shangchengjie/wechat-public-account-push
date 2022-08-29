e42576f6b961f842b6e99975faab076532bff53e
shangchengjie <49265358+shangchengjie@users.noreply.github.com>
2022-08-29 11:30:18 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wx9b6a0c2ae557c574",
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "b1b703dee06f5fe9f9c455892384bdd4",
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: "Wf8m-eD-3qYg605sHSSuAnX9xf7Sqn_NgnSeh8OcF_w",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: [""],
+    USERS: ["oQE5g5xwCZAh-H0RNm24iSnFX0gI"],
      
     /**
      * 信息配置
@@ -19,16 +19,16 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "浙江",
     // 所在城市
-    CITY: "惠州",
+    CITY: "宁波",
 
     /** 生日相关 */
 
     // 生日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
     BIRTHDAYS: [
-      {"name": "老婆", "year": "1993", "date": "12-27"},
-      {"name": "家公", "year": "1993", "date": "08-09"},
+      {"name": "臭宝", "year": "1993", "date": "12-27"},
+      {"name": "爸比", "year": "1993", "date": "08-09"},
     ],
 
     /** 日期相关 */
